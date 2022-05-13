e801cc0330ea2a07496b2ea81a31ba280ca7cc90
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-05-13 23:40:49 +0200

    update

--- cmake/builder.py
+++ cmake/builder.py
@@ -1,8 +1,9 @@
+import sys
 import ctypes
 from ctypes import *
 
 if __name__ == "__main__":
-    dll = cdll.LoadLibrary("../cmake-build-debug/examples/libsine")
+    dll = cdll.LoadLibrary(sys.argv[1])
     dll.fmi2GetTypesPlatform.restype = ctypes.c_char_p
     types = dll.fmi2GetTypesPlatform()
     print(types)
