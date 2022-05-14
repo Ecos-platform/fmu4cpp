defa4f78408ce5ae483f20a8b1945d5d83061703
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-05-14 02:26:27 +0200

    refactor

--- src/model.cpp
+++ src/model.cpp
@@ -6,29 +6,30 @@
 
 using namespace fmu4cpp;
 
-class Identity : public fmu_base {
+class Model : public fmu_base {
 
 public:
-    Identity(const std::string &instanceName, const std::string &resources)
+    Model(const std::string &instanceName, const std::string &resources)
         : fmu_base(instanceName, resources) {
 
         register_int("integer", [this] { return integer; }, [this](int value) {integer = value;});
-        register_real("real", [this] { return real; }, [this](double value) {integer = value;}).setCausality(causality_t::INPUT);
+        register_real("real", [this] { return real; }, [this](double value) {integer = value;}).setCausality(causality_t::OUTPUT);
         register_bool("boolean", [this] { return boolean; }, [this](bool value) {boolean = value;});
         register_string("string", [this] { return string; }, [this](std::string value) {string = value;});
     }
 
     bool do_step(double currentTime, double dt) override {
+        real = currentTime;
         return true;
     }
 
 private:
     int integer = 0;
-    double real = 1;
+    double real = 0;
     bool boolean = false;
     std::string string;
 };
 
 std::unique_ptr<fmu_base> fmu4cpp::createInstance(const std::string &instanceName, const std::string &fmuResourceLocation) {
-    return std::make_unique<Identity>(instanceName, fmuResourceLocation);
+    return std::make_unique<Model>(instanceName, fmuResourceLocation);
 }
