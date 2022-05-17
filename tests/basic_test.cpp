0e09c438f546838b870bbdf8f55c5b23005febcf
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-05-17 20:58:59 +0200

    fix test setup

--- tests/basic_test.cpp
+++ tests/basic_test.cpp
@@ -15,6 +15,12 @@ public:
     }
 };
 
+fmu4cpp::model_info fmu4cpp::get_model_info() {
+    fmu4cpp::model_info m;
+    m.modelIdentifier = FMU4CPP_MODEL_IDENTIFIER;
+    return m;
+}
+
 std::unique_ptr<fmu4cpp::fmu_base> fmu4cpp::createInstance(const std::string &instanceName, const std::string &fmuResourceLocation) {
     return std::make_unique<Model>(instanceName, fmuResourceLocation);
 }
